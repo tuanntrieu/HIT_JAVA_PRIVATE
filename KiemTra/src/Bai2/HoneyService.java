ddb5eb0874bf668be5e57a31834b553fdc2d6bda
unknown <trieudangtuan18032003@gmail.com>
2023-01-13 20:11:27 +0700

    Kiem tra

--- KiemTra/src/Bai2/HoneyService.java
+++ KiemTra/src/Bai2/HoneyService.java
@@ -5,41 +5,56 @@ import java.util.Scanner;
 
 public class HoneyService implements HoneyInterface {
 
-	public HoneyService() {}
+	public HoneyService() {
+	}
+
 	public ArrayList<Honey> listHoney = new ArrayList<>();
 
-	public static Scanner sc=new Scanner(System.in);
+	public static Scanner sc = new Scanner(System.in);
+
 	@Override
 	public void addHoney() {
 		// TODO Auto-generated method stub
-		Honey h=new Honey();
+		Honey h = new Honey();
 		int dem;
 		do {
 			h.input();
-			dem=0;
-			for(Honey tmp:listHoney) {
-				if(tmp.getId()==h.getId()) {
+			dem = 0;
+			for (Honey tmp : listHoney) {
+				if (tmp.getId() == h.getId()) {
 					dem++;
-					
+
 				}
 			}
-		}while(dem!=0);	
+		} while (dem != 0);
 		listHoney.add(h);
 	}
 
 	public void Tile() {
-		
+
 	}
+
 	@Override
 	public void showHoney() {
 		// TODO Auto-generated method stub
-System.out.printf("%-15d ");
+		System.out.printf("%-15d ");
 	}
 
 	@Override
 	public void delHoney() {
 		// TODO Auto-generated method stub
-
+		int id = sc.nextInt();
+		sc.nextLine();
+		int dem=0;
+		for(int i=0;i<listHoney.size();i++){
+			if(listHoney.get(i).getId()==id){
+				listHoney.remove(listHoney.gei(i));
+				dem++;
+			}
+		}
+		if(dem=0){
+			System.out.println("Khong co trong danh sach");
+		}
 	}
 
 	@Override
@@ -59,8 +74,9 @@ System.out.printf("%-15d ");
 		// TODO Auto-generated method stub
 
 	}
+
 	public static void main(String[] args) {
-		HoneyService hs=new HoneyService();
+		HoneyService hs = new HoneyService();
 		hs.addHoney();
 		hs.addHoney();
 	}
